c005a7016f6e9d90d82cdb41b414af177532b251
Fan Yang <yangfan879@gmail.com>
2015-02-17 22:20:41 +0000

    treap - tree and heap, first commit

---
+++ src/treap/treap.h
@@ -0,0 +1,30 @@
+#ifndef _TREAP_H
+#define _TREAP_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef int (*cmp_func)(const void *, const void *);
+
+struct tree {
+	struct tree *p;
+	struct tree *left;
+	struct tree *right;
+	long priority;
+	void *data;
+};
+
+typedef struct {
+	cmp_func compare;
+	size_t unit_size;
+	size_t size;
+	struct tree *root;
+} treap;
+
+extern treap *tree_init(size_t unit_size, cmp_func f);
+extern treap *tree_insert(treap *t, const void *data);
+extern void *tree_search(treap *t, const void *data);
+extern treap *tree_delete(treap *t, const void *data);
+extern void tree_free(treap *t);
+
+#endif
---
+++ src/treap/treap_test.c
@@ -0,0 +1,70 @@
+#include "treap.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#define MAXSIZE (1<<19)
+int func(const void *x, const void *y)
+{
+	const int *a = (int *)x;
+	const int *b = (int *)y;
+	if(*a > *b) return 1;
+	else if(*a == *b) return 0;
+	return -1;
+}
+
+int main(void)
+{
+	treap *t;
+	int i, tmp;
+
+	srand((unsigned int)time(NULL));
+	t = tree_init(sizeof(int), func);
+	if(!t) {
+		fprintf(stderr, "failed to initialize treap\n");
+		goto FAILED;
+	}
+	int data[MAXSIZE];
+	char tested[MAXSIZE];
+
+	memset(tested, 0, sizeof(int) * MAXSIZE);
+	for(i = 0; i < MAXSIZE; i++) {
+		data[i] = i;
+		if(tree_insert(t, &i) == NULL) {
+			fprintf(stderr, "insert error\n");
+			goto FAILED;
+		}
+	}
+
+	for(i = MAXSIZE - 1; i >= 0; i--) {
+		if(tree_search(t, &i) == NULL) {
+			fprintf(stderr, "search error\n");
+			goto FAILED;
+		}
+	}
+
+	for(i = 0; i < MAXSIZE / 2; i++) {
+		tmp = (int)rand() % MAXSIZE;
+		void *res;
+		res = tree_delete(t, &tmp);
+		if(!res) {
+			if(!tested[tmp]) {
+				fprintf(stderr, "search error\n");
+				goto FAILED;
+			}
+		} else {
+			if(tested[tmp]) {
+				fprintf(stderr, "search error\n");
+				goto FAILED;
+			}
+			tested[tmp] = 1;
+		}
+	}
+	tree_free(t);
+	puts("----------passed----------");
+	exit(EXIT_SUCCESS);
+FAILED:
+	puts("!!!!!!!!!!failed!!!!!!!!!!");
+	exit(EXIT_FAILURE);
+}
