443d0234dae500cfb6abb22e7cbda16179a18e11
Fan Yang <yangfan879@gmail.com>
2015-03-09 18:44:20 +0000

    change test code

--- src/stack/stack_test.c
+++ src/stack/stack_test.c
@@ -2,22 +2,28 @@
 #include <stdlib.h>
 #include "stack.h"
 
+#define MAXSIZE (1<<19)
+
 int main(void)
 {
 	int i, tmp;
 	stack *s;
 
 	if((s = stack_init(sizeof(int))) == NULL) {
-		fprintf(stderr, "failed to initialize stack\n");
-		exit(EXIT_FAILURE);
+		goto FAILED;
 	}
-	for(i = 0; i < 100; i++) {
+	for(i = 0; i < MAXSIZE; i++) {
 		push(s, &i);
 	}
-	while(pop(s, &tmp)) {
-		printf("%d ", tmp);
+	for(i = MAXSIZE - 1; i >= 0; i--) {
+		pop(s, &tmp);
+		if(tmp != i) goto FAILED;
 	}
-	puts("");
 	stack_free(s);
+	puts("-----passed-----");
+
 	return 0;
+FAILED:
+	fprintf(stderr, "!!!!!failed!!!!!\n");
+	exit(EXIT_FAILURE);
 }
